9e85844255b327a3e074b2481ef5deaf73f02c42
Prabir Singh <prabirsinghdigital@gmail.com>
2023-01-06 23:07:34 +0530

    linked database

---
+++ components/header/Ad.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+import styles from './styles.module.scss'
+export default function Ad() {
+  return (
+    <Link href='/browse'>
+      <div className={styles.ad}>ad</div>
+    </Link>
+  )
+}
